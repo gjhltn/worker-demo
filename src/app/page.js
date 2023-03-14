5f54a050d65e8b69e16f39b4394859a37ca75a06
gswirrl <guy@swirrl.com>
2023-03-14 00:55:29 +0000

    worker

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,27 @@
+"use client"
+
+import { useEffect, useRef, useCallback } from 'react'
+
 export default function Home() {
+  const workerRef = useRef()
+
+  useEffect(() => {
+    workerRef.current = new Worker(new URL('../../worker.js', import.meta.url))
+    workerRef.current.onmessage = (event) =>
+      alert(`WebWorker Response => ${event.data}`)
+    return () => {
+      workerRef.current?.terminate()
+    }
+  }, [])
+
+  const handleWork = useCallback(async () => {
+    workerRef.current?.postMessage(100000)
+  }, [])
+
   return (
-    <main>
-      Hello world
-    </main>
+    <>
+      <p>Do work in a WebWorker!</p>
+      <button onClick={handleWork}>Calculate PI</button>
+    </>
   )
 }
---
+++ src/utils/pi.js
@@ -0,0 +1,9 @@
+export default function pi(n) {
+    let v = 0
+    for (let i = 1; i <= n; i += 4) {
+      // increment by 4
+      v += 1 / i - 1 / (i + 2) // add the value of the series
+    }
+    return 4 * v // apply the factor at last
+  }
+  
